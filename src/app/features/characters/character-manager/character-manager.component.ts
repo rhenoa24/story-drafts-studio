73ae152acaa76b047bf4cbd36125a2d1b9d0bfdd
Alyssa Rhenoa Nicole Bautista <>
2026-08-13 17:16:01 +0800

    Added icon for Add Character

--- src/app/features/characters/character-manager/character-manager.component.ts
+++ src/app/features/characters/character-manager/character-manager.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { StoryDataService } from '../../../core/services/story-data.service';
 import { EditModeService } from '../../../core/services/edit-mode.service';
+import { IconComponent } from '../../../shared/icon/icon.component';
 
 @Component({
   selector: 'app-character-manager',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, IconComponent],
   templateUrl: './character-manager.component.html',
   styleUrl: './character-manager.component.scss',
 })
